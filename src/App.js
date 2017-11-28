7ea128c113fa5fa743f11aa3a27c6107e124f611
cliffgor <clierozigy@gmail.com>
2017-11-29 00:57:59 +0300

    Modifications made; 1) Created the react app 2) Added a function gridcell to display a cube 3) Added a function that displays the gridcell in rows of three

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function GridCell(){
+  var style = {
+    width:20,
+    height:20,
+    border:'1px solid black',
+    backgroundColor:'red'
+  }
+  return (
+    <div style = {style}></div>
+  )
+}
+
+
+function GridRow(){
+  return (
+    <div>
+      <GridCell/>
+      <GridCell/>
+      <GridCell/>
+    </div>
+  )
+}
+
+export default GridRow;
