e7c052bac2481859d0b2d4f6a7a9e44b71f8c256
cliffgor <clierozigy@gmail.com>
2017-11-29 03:34:13 +0300

    modifications made by adding a for loop that displays a 2 dimensional grid of the boxes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function GridCell(){
+function GridCell(props){
   var style = {
     width:20,
     height:20,
     border:'1px solid black',
-    backgroundColor:'red'
+    backgroundColor:props.cell
   }
   return (
     <div style = {style}></div>
@@ -15,27 +15,31 @@ function GridCell(){
 }
 
 
-function GridRow(){
+function GridRow(props){
   var style = {
     display:'flex'
   }
    
   return (
     <div style = {style}>
-      <GridCell/>
-      <GridCell/>
-      <GridCell/>
+      {
+        props.row.map( (cell) =>{
+          return <GridCell cell = {cell}/>
+        } )
+      }
     </div>
   )
 }
 
 
-function Grid() {
+function Grid(props) {
   return (
     <div>
-      <GridRow/>
-      <GridRow/>
-      <GridRow/>      
+      {
+        props.grid.map( (row) => {
+          return <GridRow row = {row}/>
+        })
+      }
     </div>
   )
 }
@@ -44,9 +48,15 @@ function Grid() {
 class Game extends React.Component{
   constructor(props){
     super(props)
+    var grid = []
+    for(let i = 0; i < 20; i++){
+      grid.push(new Array(30).fill('red'))
+    }
+    this.state = {grid:grid}
   }
   render(){
-    return(<Grid/>)
+    return(
+    <Grid grid = {this.state.grid}/>)
   }
 }
 
